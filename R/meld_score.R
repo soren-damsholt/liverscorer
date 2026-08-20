01d2cbeafac377eb100604e8c6342edf932c8af1
Søren Damsholt <soerdm@clin.au.dk>
2026-08-20 18:35:26 +0200

    Updated MELD score to set values below 1 to 1 mg/dL. Added fail conditions if either dialysis or unit is not formatted correct. added tests for vectorisation and fail conditions.

--- R/meld_score.R
+++ R/meld_score.R
@@ -7,11 +7,11 @@
 #' @param unit Parameter setting if the measurements are based on "US" units (mg/dL) or SI units (μmol/L). Default value is "US". If the setting is changed to "SI", creatinine and bilirubin will be converted to mg/dL by multiplying their respective conversion factor.
 #' @param creatinine_conversion_factor Factor multiplied to creatinine to convert from μmol/L to mg/dL (default = 0.0113).
 #' @param bilirubin_conversion_factor Factor multiplied to bilirubin to convert from μmol/L to mg/dL (default = 0.0584).
-#' @param creatinine_treshold_si Upper threshold for high creatinine (μmol/L), causing a warning if input is above (default = 200).
+#' @param creatinine_threshold_si Upper threshold for high creatinine (μmol/L), causing a warning if input is above (default = 200).
 #' @param bilirubin_threshold_si Upper threshold for high bilirubin (μmol/L), causing a warning if input is above (default = 360).
-#' @param creatinine_treshold_us Upper threshold for high creatinine (mg/dL), causing a warning if input is higher (default = 2.26).
+#' @param creatinine_threshold_us Upper threshold for high creatinine (mg/dL), causing a warning if input is higher (default = 2.26).
 #' @param bilirubin_threshold_us Upper threshold for high bilirubin (mg/dL), causing a warning if input is higher (default = 21.05).
-#' @param inr_treshshold Upper threshold for INR, causing a warning if input is higher (default = 3.0).
+#' @param inr_threshshold Upper threshold for INR, causing a warning if input is higher (default = 3.0).
 #'
 #' @returns Function returns a calulated MELD value, rounded to nearest integer.
 #' @export
@@ -34,36 +34,47 @@
 #'   meld(creatinine = 120, bilirubin = 80, inr = 2.0, unit = "SI") # should return 23
 meld <- function(creatinine, bilirubin, inr, dialysis = "no", unit = "US",
                  creatinine_conversion_factor = 0.0113, bilirubin_conversion_factor = 0.0584,
-                 creatinine_treshold_si = 200, bilirubin_threshold_si = 360,
-                 creatinine_treshold_us = 2.26, bilirubin_threshold_us = 21.05, 
-                 inr_treshshold = 3.0){
-  
+                 creatinine_threshold_si = 200, bilirubin_threshold_si = 360,
+                 creatinine_threshold_us = 2.26, bilirubin_threshold_us = 21.05, 
+                 inr_threshshold = 3.0){
+  #Input check
+  if(!unit %in% c("US", "SI")){
+    stop("unit must be either 'US' or 'SI'")
+  }
+  if(!dialysis %in% c("yes", "no")){
+    stop("dialysis must be either 'yes' or 'no'")
+  }
   # Safety check
-  if(unit == "SI" && (creatinine > creatinine_treshold_si |
+  if(unit == "SI" && any(creatinine > creatinine_threshold_si |
                       bilirubin > bilirubin_threshold_si |
-                      inr > inr_treshshold)){
+                      inr > inr_threshshold,
+                      na.rm = TRUE)){
     warning("SI units detected: some lab values are high, consider if input is correct?")
   }
-  else if(unit == "US" && (creatinine > creatinine_treshold_us |
+  else if(unit == "US" && any(creatinine > creatinine_threshold_us |
                            bilirubin > bilirubin_threshold_us |
-                           inr > inr_treshshold)){
+                           inr > inr_threshshold,
+                           na.rm = TRUE)){
     warning("US units detected: some lab values are high, consider if input is correct?")
   }
-  # If dialysis
-  if(dialysis == "yes" && unit == "SI"){
-    creatinine <- 4.0
-    bilirubin <- bilirubin * bilirubin_conversion_factor
-  }
-  else if(dialysis == "no" && unit == "SI"){
+  
+  # Convert SI units to mg/dL
+  if(unit == "SI"){
     creatinine <- creatinine * creatinine_conversion_factor
     bilirubin <- bilirubin * bilirubin_conversion_factor
-    
   }
-  else if(dialysis == "yes" && unit == "US"){
+  
+  # If dialysis
+  if(dialysis == "yes"){
     creatinine <- 4.0
   }
-  # Calculate meld
   
+  # Set MELD minimum values
+  creatinine <- pmax(creatinine, 1)
+  bilirubin <- pmax(bilirubin, 1)
+  inr <- pmax(inr, 1)
+  
+  # Calculate meld
   meld <- round((0.957 * log(creatinine) +
      0.378 * log(bilirubin)+
      1.120 * log(inr) +
--- tests/testthat/test-meld_function.R
+++ tests/testthat/test-meld_function.R
@@ -1,7 +1,72 @@
-testthat::test_that("Testing that meld score calculates correctly, validated against the online calculator at: https://www.mdcalc.com/calc/10437/model-end-stage-liver-disease-meld#evidence", {
-  testthat::expect_equal(liverscorer:::meld(creatinine = 120, bilirubin = 80, inr = 2.0, unit = "SI", dialysis = "no"), 23)
-  testthat::expect_equal(liverscorer:::meld(creatinine = 120, bilirubin = 80, inr = 2.0, unit = "SI", dialysis = "yes"), 33)
-  testthat::expect_equal(liverscorer:::meld(creatinine = 1.2, bilirubin = 5, inr = 2.0, unit = "US", dialysis = "no"), 22)
-  testthat::expect_equal(liverscorer:::meld(creatinine = 1.2, bilirubin = 5, inr = 2.0, unit = "US", dialysis = "yes"), 34)
+test_that("Testing that meld score calculates correctly (US + no dialysis)", {
+  calculated <- meld(creatinine = 1.2, bilirubin = 5, inr = 2.0, unit = "US", dialysis = "no")
+  expected  <- round((0.957 * log(1.2) +
+                       0.378 * log(5) +
+                       1.120 * log(2) +
+                       0.643) * 10)
+  expect_equal(calculated, expected)
   
 })
+
+test_that("Testing that meld score calculates correctly (US + dialysis)", {
+  calculated <- meld(creatinine = 1.2, bilirubin = 5, inr = 2.0, unit = "US", dialysis = "yes")
+  expected  <- round((0.957 * log(4) +
+                       0.378 * log(5) +
+                       1.120 * log(2) +
+                       0.643) * 10)
+  expect_equal(calculated, expected)
+})
+
+test_that("Testing that meld score calculates correctly (SI + no dialysis)", {
+  calculated <- meld(creatinine = 120, bilirubin = 80, inr = 2.0, unit = "SI", dialysis = "no")
+  expected  <- round((0.957 * log(120*0.0113) +
+                       0.378 * log(80*0.0584) +
+                       1.120 * log(2) +
+                       0.643) * 10)
+  expect_equal(calculated, expected)
+  
+})
+
+test_that("Testing that meld score calculates correctly (SI + dialysis)", {
+  calculated <- meld(creatinine = 120, bilirubin = 80, inr = 2.0, unit = "SI", dialysis = "yes")
+  expected  <- round((0.957 * log(4) +
+                       0.378 * log(80*0.0584) +
+                       1.120 * log(2) +
+                       0.643) * 10)
+  expect_equal(calculated, expected)
+  
+})
+
+test_that("MELD values below 1 are set to 1", {
+  calculated <- meld(creatinine = 0.5, bilirubin = 0.5, inr = 0.5,unit = "US", dialysis = "no")
+  
+  expected <- round((0.957 * log(1) +
+                       0.378 * log(1) +
+                       1.120 * log(1) +
+                       0.643) * 10)
+  expect_equal(calculated, expected)
+  
+})
+
+test_that("MELD is vectorized", {
+  calculated <- meld(creatinine = c(0.5, 1, 2), bilirubin = c(0.5, 1, 2), inr = c(0.5, 1, 2), unit = "US")
+  expect_length(calculated, 3)
+  expect_true(all(is.finite(calculated)))
+})
+
+test_that("Invalid units produces error and stops MELD function", {
+  expect_error(
+    meld(1.2, 5, 2, unit = "Si"),
+    "unit must be either 'US' or 'SI'"
+  )
+})
+
+test_that("Invalid dialysis setting produces an error",{
+  expect_error(
+    meld(1.2, 5, 2, dialysis = "maybe"),
+    "dialysis must be either 'yes' or 'no'"
+  )
+})
+
+
+
